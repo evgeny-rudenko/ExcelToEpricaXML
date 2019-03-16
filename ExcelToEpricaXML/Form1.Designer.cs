7665ed550512371e3f8a51fa494fc1e1a1eab75a
Evgeny Rudenko <rudenkoevgeny@gmail.com>
2019-03-16 14:31:15 +1000

    first commit

---
+++ ExcelToEpricaXML/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ExcelToEpricaXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "C:\\Users\\evgeny\\source\\repos\\ExcelToEpricaXML\\ExcelToEpricaXML\\bin\\Debug\\заказ.xl" +
+    "s";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Дефектура";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 197);
+            this.progressBar1.Maximum = 1500;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(196, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 232);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Excel To Eprica XML";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ ExcelToEpricaXML/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Xml;
+using System.IO;
+
+namespace ExcelToEpricaXML
+{
+   
+
+    public partial class Form1 : Form
+    {
+        private static Excel.Workbook MyBook = null;
+        private static Excel.Application MyApp = null;
+        private static Excel.Worksheet ErSheet = null;
+
+
+        /// <summary>
+        /// Функция необходима для того, чтобы убать первую строчку  описания XML. 
+        /// ePrica не обрабатывает файл отличный от <XML> </XML>
+        /// </summary>
+        /// <param name="xmlPath"></param>
+        public void removeXMLdeclaration(string xmlPath)
+        {
+            try
+            {
+                //Grab file
+                StreamReader sr = new StreamReader(xmlPath);
+
+                //Read first line and do nothing (i.e. eliminate XML declaration)
+                sr.ReadLine();
+                string body = null;
+                string line = sr.ReadLine();
+                while (line != null) // read file into body string
+                {
+                    body += line + "\n";
+                    line = sr.ReadLine();
+                }
+                sr.Close(); //close file
+
+                //Write all of the "body" to the same text file
+                System.IO.File.WriteAllText(xmlPath, body);
+            }
+            catch (Exception e3)
+            {
+                MessageBox.Show(e3.Message);
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop,false);
+            foreach (string file in files)
+            {
+                textBox1.Text = file;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fname = "";
+            fname = textBox1.Text;
+            if (fname.Length == 0)
+                 return;
+
+            if (!File.Exists(fname))
+            {
+                MessageBox.Show("Не могу найти файл ");
+                return;
+            }
+
+            try
+            {
+                MyApp = new Excel.Application();
+                MyApp.Visible = true;
+
+
+                MyBook = MyApp.Workbooks.Open(fname);
+
+                ErSheet = (Excel.Worksheet)MyBook.Sheets[1];
+            }
+            catch (Exception e3)
+            {
+                MessageBox.Show(e3.Message);
+                return;
+            }
+            XmlWriterSettings settingsxml = new XmlWriterSettings();
+            String defpath = "defektura.xml";
+
+
+            settingsxml.Indent = true;
+            #region Шапка XML  файла дефектуры пример
+            /*
+             <HEADER>
+            <ROW>
+            <ID_DEFECTURA>659fa090-24d4-473f-adc3-ff73c9557e3e</ID_DEFECTURA>
+            <DOC_DATE>2019-03-10T17:59:18.597</DOC_DATE>
+            <ID_CONTRACTOR_GLOBAL>1fb6f806-63bc-4ef0-afca-736c5dc1e7fa</ID_CONTRACTOR_GLOBAL>
+            <CONTRACTOR_NAME>ООО "Волга" ЦО</CONTRACTOR_NAME>
+            <CONTRACTOR_CODE>
+            </CONTRACTOR_CODE>
+            </ROW>
+            </HEADER>
+
+            */
+            #endregion 
+            using (XmlWriter writer = XmlWriter.Create("defektura.xml", settingsxml))
+            {
+                writer.WriteStartElement("XML");
+                writer.WriteStartElement("HEADER");
+                writer.WriteStartElement("ROW");
+                writer.WriteElementString("ID_DEFECTURA",System.Guid.NewGuid().ToString());
+                writer.WriteElementString("DOC_DATE", DateTime.Now.ToString("yyyy-MM-ddThh:mm:ss"));
+                writer.WriteElementString("ID_CONTRACTOR_GLOBAL", "1fb6f806-63bc-4ef0-afca-736c5dc1e7fa");
+                writer.WriteElementString("CONTRACTOR_NAME","ООО Здоровье");
+                writer.WriteElementString("CONTRACTOR_CODE","");
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+
+                #region Строка дефектуры пример
+                /*
+                 <ROWS>
+                 <ROW>
+                   <ID_GOODS_GLOBAL>b0a1680c-501f-4233-97f0-fd18ffb695f9</ID_GOODS_GLOBAL>
+                <GOODS_NAME>La Roche-Posay Гиалу В5 крем для глаз 15мл</GOODS_NAME>
+                <PRODUCER_NAME>Косметик актив продюксьон</PRODUCER_NAME>
+                <QTY_REMAIN>0.0000</QTY_REMAIN>
+                <QTY_MIN>0.0000</QTY_MIN>
+                  <LAST_PRICE_SAL>1680.0000</LAST_PRICE_SAL>
+                <LAST_PRICE_SUP>1345.2000</LAST_PRICE_SUP>
+                <LAST_SUPPLIER_NAME>Бьютикс-Восток ООО</LAST_SUPPLIER_NAME>
+                </ROW> 
+                */
+                #endregion
+                writer.WriteStartElement("ROWS");
+                for (int index = 14; index <= 1500; index++)
+                {
+                    
+                    String GoodName = ErSheet.get_Range("A" + index.ToString(), "A" + index.ToString()).Cells.Value;
+                    if (GoodName == "")
+                        continue;
+                    Excel.Range rngremain = ErSheet.get_Range("C" + index.ToString(), "C" + index.ToString());
+                    if (rngremain.Text == "")
+                        continue;
+                    float remain = float.Parse(rngremain.Text);
+
+
+                    Excel.Range rngmin = ErSheet.get_Range("F" + index.ToString(), "F" + index.ToString());
+                    if (rngmin.Text == "")
+                        continue;
+                    float min = float.Parse(rngmin.Text);
+
+
+                    writer.WriteStartElement("ROW");
+                    writer.WriteElementString("ID_GOODS_GLOBAL", System.Guid.NewGuid().ToString());
+                    writer.WriteElementString("GOODS_NAME", GoodName);
+                    writer.WriteElementString("PRODUCER_NAME", "Производитель");
+
+                    
+                    writer.WriteElementString("QTY_REMAIN", remain.ToString().Replace(",","."));
+
+
+                    writer.WriteElementString("QTY_MIN", min.ToString().Replace(",","."));
+
+                    writer.WriteElementString("LAST_PRICE_SAL", "0.000");
+                    writer.WriteElementString("LAST_PRICE_SUP", "0.000");
+                    writer.WriteElementString("LAST_SUPPLIER_NAME", "Поставщик");
+                    writer.WriteEndElement();
+
+                    progressBar1.Value = index;
+
+
+                   
+                }
+                writer.WriteEndElement(); //ROWS
+                writer.WriteEndElement(); //XML
+                writer.Flush();
+
+               
+
+            }
+            removeXMLdeclaration(defpath);
+            MessageBox.Show("Дефектура готова");
+        }
+    }
+}
